aac13f31efeceb72e010ef1612bc4eeb16861dbb
raitavastu <ravastu@tlu.ee>
2016-02-01 23:49:40 +0200

    color changeing updated

--- clock.js
+++ clock.js
@@ -1,17 +1,39 @@
 window.onload = function(){
     var clock = document.getElementById('clock');
+    var name  = document.getElementById('name');
+    var body = document.getElementsByTagName('body');
+
+    clock.style.textAlign = 'center';
+    clock.style.marginTop = '25%';
+    name.style.textAlign = 'center';
     writeTime();
 
     window.setInterval(function(){
       writeTime();
     }, 1000);
 
-    var colors = ['red', 'blue', 'green', 'yellow'];
+
     clock.addEventListener('click', function(){
-      var color = colors[Math.floor(Math.random()*colors.length)]; // StackOverflow
-      clock.style.color = color;
+      clock.style.color = randomColor();
+    });
+
+    document.addEventListener('keydown', function(){
+      if(event.keyCode == "38"){ // up
+        body.style.background = randomColor();
+      } else if(event.keyCode == "40"){ // down
+
+      } else if(event.keyCode == "37"){ // left
+
+      } else if(event.keyCode == "39"){ // right
+
+      }
     });
 };
+
+function randomColor(){
+  var colors = ['red', 'blue', 'green', 'yellow', 'black'];
+  return colors[Math.floor(Math.random()*colors.length)];
+}
 
 function writeTime(){
   var today = new Date();
@@ -20,6 +42,7 @@ function writeTime(){
   var m = today.getMinutes();
   var s = today.getSeconds();
 
+  h = setZeroBefore(h);
   m = setZeroBefore(m);
   s = setZeroBefore(s);
 
