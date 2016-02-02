20f922033dd883a5472e7d00d7b94787ce68a726
naaber <martnaa@greeny.cs.tlu.ee>
2016-02-03 00:27:20 +0200

    seconds work

--- clock.js
+++ clock.js
@@ -1,55 +1,86 @@
 window.onload = function(){
 
-  var clock = document.getElementById("clock");
-
-  clock.addEventListener('click', function(){
-
-    console.log("click");
-
-  });
-
-  window.addEventListener('keypress', function(){
-
-    console.log(event);
-
-    if(event.charCode == 13){
-
-      alert('enter');
-    }
-
-  });
-
-  writeDate();
+  var clock_hour = document.getElementById("clock_hour");
+  var clock_minute = document.getElementById("clock_minute");
+  var clock_second = document.getElementById("clock_second");
 
   window.setInterval(function(){
 
-    writeDate();
+    writeSecond();
 
-  }, 1000); //millisekundid, 1000ms = 1s
-
+  }, 1000);
 
 };
 
-function writeDate(){
 
-  var today = new Date();
 
-  var hours = today.getHours();
-  var minutes = today.getMinutes();
-  var seconds = today.getSeconds();
+function writeSecond(){
 
-  clock.innerHTML = setZeroBefore(hours) + ':' + setZeroBefore(minutes) + ':' + setZeroBefore(seconds);
+  clock_second.innerHTML = "SEKUND";
+  clock_second.innerHTML += "<br>";
 
+  for(i = 1; i < 61; i++){
+
+    if(i%10 === 0){
+
+      clock_second.innerHTML += makeNumberBold(setZeroBefore(i));
+      clock_second.innerHTML += "<br>";
+
+    }else{
+
+    clock_second.innerHTML += makeNumberBold(setZeroBefore(i));
+
+    }
+  }
 }
 
-//lisab nulli, kui arv on kümnest väiksem
 function setZeroBefore(number){
 
-  if(number <10){
-    number ='0' + number;
+  if(number < 10){
+
+    number = '0' + number;
 
   }
 
   return number;
 
 }
+
+function makeNumberBold(number){
+
+  var today = new Date();
+  var second = today.getSeconds();
+
+  if(number === setZeroBefore(second)){
+
+    number = "<b>" + number + "<b>";
+
+  }
+
+  return number;
+
+}
+
+
+
+
+/*
+
+clock_hour.addEventListener('click', function(){
+
+  console.log("click");
+
+});
+
+window.addEventListener('keypress', function(){
+
+  console.log(event);
+
+  if(event.charCode == 13){
+
+    alert('enter');
+  }
+
+});
+
+*/
