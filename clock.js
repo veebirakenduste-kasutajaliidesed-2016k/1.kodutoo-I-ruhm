ab473cd98ec1b92bf9bbd0b3caba8808275a206d
Martin Viidik <martvii@greeny.cs.tlu.ee>
2016-02-02 19:40:38 +0200

    Basic clock with user interface

---
+++ clock.js
@@ -0,0 +1,37 @@
+// clock.js
+window.onload = function(){
+
+  var clock = document.getElementById('clock');
+
+
+  // enne timeouiti kirjutan ühe korra ära
+  writeDate();
+// millisekundid 1000ms = 1s
+  window.setInterval(function(){
+
+    // Iga ootaja järel käivitatakse
+    writeDate();
+
+  }, 1000);
+
+};
+
+function writeDate(){
+
+  var today = new Date();
+
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+
+  clock.innerHTML = setZeroBefore(hours) + ':' + setZeroBefore(minutes) + ':' + setZeroBefore(seconds);
+
+}
+
+function setZeroBefore(number) {
+  if(number < 10){
+    number = '0' + number;
+  }
+
+  return number;
+}
