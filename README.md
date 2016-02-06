b2b91f6c0f646501b3328229b05da6494d3e8309
Janek Kossinski <janekos@greeny.cs.tlu.ee>
2016-02-06 20:14:56 +0200

    oops i deleted everything

---
+++ README.md
@@ -0,0 +1,29 @@
+# 1. kodutoo (I ruhm) – Lauakell
+
+Kujunda tunnis alustatud kellanaide vastavalt maitsele voi kindlale teemale, mahutades kella taisekraanile, et saaks kasutada lauakella voi ekraanisaastja asemel. Selleks, et see sobiks paljudele ekraanidele, kasuta kujunduse loomisel protsendilisi vaartusi (nt width: 100%; ) voi voimalda kella suurust kasutajal muuta.
+
+## Nouded
+
+1. Veebirakendus tootab, st naitab kella ja kuupaeva
+1. Vastavalt kasutaja tegevusele on voimalik vahemalt kahte lauakella atribuuti muuta (nt suurus, varv, asukoht ...)
+1. Kell on originaalne ning kasutajaliides on maitsekalt kujundatud
+1. Autori ees- ja perenimi on valja toodud
+
+## Moned ideed voimalikeks taiendusteks (lihtsalt, et mote hakkaks jooksma)
+
+* Vahetuda voivad taustapildid, voib mangida taustal mingi muusika
+* Taustale klikkides muudetakse kella taustavari
+* Kellale klopsides muudetakse numbrite varvi voi numbrite suurust;
+* Iga numbri suurust saab eraldi muuta
+* Nooleklahvidega saab kella ekraanil liigutada
+* Kusi enne kella naitamise alustamist kasutajalt tema lemmikvarv ja tee sellest lahtudes midagi
+* Muuda kella ja tausta varvi vastavalt ajale (paev/oo)
+* Kella suurus ja asukoht soltuvad hiire asukohast ekraanil
+* Akki voiks tegu olla hoopis analoogkellaga
+* Kirjatuupi saab ka muuta [google.com/fonts](https://www.google.com/fonts)
+
+## Abimaterjal
+
+* sundmuste loetelu [HTML DOM Events](http://www.w3schools.com/jsref/dom_obj_event.asp)
+* ajal pohinevad sundmused [JavaScript Timing Events](http://www.w3schools.com/js/js_timing.asp)
+* CSSi muutmine [HTML DOM Style Object](http://www.w3schools.com/jsref/dom_obj_style.asp)
---
+++ clock.js
@@ -0,0 +1,92 @@
+
+ window.onload = function(){
+
+   var clock = document.getElementById('clock');
+   var count1 = 0;
+   clock.style.color = "magenta";
+   colors = ["red", "black", "green", "blue", "yellow", "cyan"];
+   var color1 = 0;
+
+	clock.addEventListener('click', function(){
+		
+		color1 = colors[count1];
+		console.log(color1);
+		count1++;
+		clock.style.color = color1;
+	
+		if(count1 > 5){
+			
+			count1 = 0;
+			
+		}
+
+	});
+
+   /*window.addEventListener('keypress', function(event){
+     console.log(event);
+
+     if(event.keyCode == 13){
+       alert('enter');
+     }
+
+   });*/
+
+   // enne timeouti kirjutan uhe korra ara
+   writeDate();
+
+   window.setInterval(function(){
+
+     // iga ooteaja jarel kaivitatakse
+     writeDate();
+
+   }, 500);// millisekundid - 1000ms = 1s
+   
+   var count = 0;
+	
+	window.setInterval(function(){
+	
+		if(count > 9){
+			count = 0;
+		}
+		
+		//console.log(count);
+		var url = json.data[count].images.original.url;
+		
+		document.getElementById("giphy").style.backgroundImage = 'url('+url+')';
+		
+		//console.log(url);
+		
+		count++;
+     
+
+   }, 1000);// millisekundid - 1000ms = 1s
+	
+
+ };
+
+function writeDate(){
+
+	var today = new Date();
+	
+	months = ["Jaanuar","Veebruar","Marts","Aprill","Mai","Juuni","Juuli","August","September","Oktoober","November","Detsember"];
+	
+	var hours = today.getHours();
+	var minutes = setZeroBefore(today.getMinutes());
+	var seconds = today.getSeconds();
+	var month = today.getMonth();
+	var year = today.getFullYear();
+	var date = today.getDate();
+	
+	seconds = setZeroBefore(seconds);
+
+	clock.innerHTML = "<p id='kell'>" + setZeroBefore(hours) + ':'+ minutes + ':' + seconds + "</p><br><p id='kp'>" + date + ". " + months[month] + " " + year + "</p>";
+
+}
+
+function setZeroBefore(number) {
+  if(number < 10){
+    number = '0' + number;
+  }
+
+  return number;
+}
