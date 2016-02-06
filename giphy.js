2fb5004df473a55b3afc6085f866d601fd58a247
Janek Kossinski <janekos@greeny.cs.tlu.ee>
2016-02-06 20:10:03 +0200

    clean up

--- giphy.js
+++
@@ -1,4 +0,0 @@
- window.onload = function(){
-	var xhr = document.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5");
-	xhr.done(function(data) { console.log("success got data", data); });
- };
