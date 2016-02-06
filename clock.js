88c74977aed1fb89096823658a55f16aa0882de2
Ruslan Jegorov <rusljeg@greeny.cs.tlu.ee>
2016-02-06 20:36:49 +0200

    nuppud

--- clock.js
+++ clock.js
@@ -11,6 +11,15 @@ window.onload = function(){
     writeDate();
 
   }, 1000); //millisekundid - 1000ms = 1s
+  
+  document.getElementById('background1').addEventListener('click', background1);
+  document.getElementById('background2').addEventListener('click', background2);
+  document.getElementById('background3').addEventListener('click', background3);
+  document.getElementById('background4').addEventListener('click', background4);
+  document.getElementById('background5').addEventListener('click', background5);
+  
+  document.getElementById('bigger').addEventListener('click', bigger);
+  document.getElementById('normal').addEventListener('click', normal);
 
 };
 
@@ -18,12 +27,12 @@ function writeDate(){
 
  // tänane kp: Mon Feb 01 2016 12:42:44 GMT+0200 (FLE Standard Time)
  var today = new Date();
-
- var hours = today.getHours();
- var minutes = today.getMinutes();
- var seconds = today.getSeconds();
-
- clock.innerHTML = setZeroBefore(hours) + ':'+ setZeroBefore(minutes) + ':' + setZeroBefore(seconds);
+ 
+ var day = today.getDay();
+ var month = today.getMonth() + 1;
+ var year = today.getFullYear();
+ 
+ document.getElementById("date").innerHTML = setZeroBefore(day) + "." + setZeroBefore(month) + "." + setZeroBefore(year);
 
 }
 //lisab nulli kui arv on 10st vahem
@@ -34,3 +43,35 @@ function setZeroBefore(number){
 
   return number;
 }
+
+
+function background1(){
+  document.body.style.background = "#33FFFF";
+}
+
+function background2(){
+  document.body.style.background = "#6600FF";
+}
+
+function background3(){
+  document.body.style.background = "#CC00FF";
+}
+
+function background4(){
+  document.body.style.background = "#00FF33";
+}
+
+function background5(){
+  document.body.style.background = "white";
+}
+
+function bigger(){
+  document.getElementById("date").style.fontSize = "150px";
+  document.getElementById("script").style.width = "650px";
+}
+function normal(){
+  document.getElementById("date").style.fontSize = "100px";
+  document.getElementById("script").style.width = "450px";
+}
+
+
