3b65c3281cbfda582931d7b990f51d2379a85367
raitavastu <ravastu@tlu.ee>
2016-02-06 17:52:02 +0200

    added font size and font family change

--- clock.js
+++ clock.js
@@ -1,7 +1,20 @@
 window.onload = function(){
     var clock = document.getElementById('clock');
     var name  = document.getElementById('name');
-    var body = document.getElementsByTagName('body');
+    var fontSelect = document.getElementById('fontSelection');
+
+    WebFontConfig = {
+        google: { families: [ 'Oswald::latin', 'Indie+Flower::latin', 'Dosis:700:latin', 'Lobster::latin' ] }
+      };
+      (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+          '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+      })();
 
     clock.style.textAlign = 'center';
     clock.style.marginTop = '25%';
@@ -12,27 +25,28 @@ window.onload = function(){
       writeTime();
     }, 1000);
 
+    fontSelect.addEventListener("change", function(){
+      clock.style.fontSize = fontSelect.value;
+    });
 
     clock.addEventListener('click', function(){
       clock.style.color = randomColor();
+      clock.style.fontFamily = randomFont();
     });
 
-    document.addEventListener('keydown', function(){
-      if(event.keyCode == "38"){ // up
-        body.style.background = randomColor();
-      } else if(event.keyCode == "40"){ // down
-
-      } else if(event.keyCode == "37"){ // left
-
-      } else if(event.keyCode == "39"){ // right
-
-      }
+    document.body.addEventListener('click', function(){
+        document.body.style.background = randomColor();
     });
 };
 
 function randomColor(){
   var colors = ['red', 'blue', 'green', 'yellow', 'black'];
   return colors[Math.floor(Math.random()*colors.length)];
+}
+
+function randomFont(){
+  var fonts = ['Oswald, latin', 'Indie+Flower, latin', 'Dosis, latin', 'Lobster, latin'];
+  return fonts[Math.floor(Math.random()*fonts.length)];
 }
 
 function writeTime(){
@@ -42,9 +56,12 @@ function writeTime(){
   var m = today.getMinutes();
   var s = today.getSeconds();
 
+
   h = setZeroBefore(h);
   m = setZeroBefore(m);
   s = setZeroBefore(s);
+
+
 
   clock.innerHTML = h + ":" + m + ":" + s;
 }
